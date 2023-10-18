cbafec50f90eac11a02a0d48f5400eba099d03b7
serubeltran <142681418+serubeltran@users.noreply.github.com>
2023-10-18 07:55:26 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const navMini = document.querySelector(".icono1");
-const navMenu = document.querySelector(".nav-contenedor");
-
-navMini.addEventListener("click", () => {
-    navMenu.classList.toggle("nav_menu_visible");
-})
